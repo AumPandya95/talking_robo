0f54254620b06cd75fed0fb02502fc9d1c7085c9
Aum Pandya <aumpandya.95@gmail.com>
2024-03-29 18:11:32 +0530

    Changed the way model is loaded

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import os
+import time
 from pathlib import Path
 from dotenv import load_dotenv
 from fastapi import FastAPI
@@ -10,15 +11,24 @@ app = FastAPI()
 env_path = os.path.join(Path.cwd().parent, ".env")
 load_dotenv(env_path)
 
-TOKENIZER = AutoTokenizer.from_pretrained("google/gemma-2b-it",
-                                          device_map="auto",
-                                          token=os.environ.get("hugging_face_token"))
-MODEL = AutoModelForCausalLM.from_pretrained("google/gemma-2b-it",
-                                             device_map="auto",
-                                             token=os.environ.get("hugging_face_token"),
-                                             use_cache=False
-                                             # quantization_config=quantization_config
-                                            )
+
+def load_model():
+    start_time = time.time()
+    tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b-it",
+                                            #   device_map="auto",
+                                              token=os.environ.get("hugging_face_token")
+                                              )
+    model = AutoModelForCausalLM.from_pretrained("google/gemma-2b-it",
+                                                #  device_map="auto",
+                                                 token=os.environ.get("hugging_face_token"),
+                                                 use_cache=True
+                                                 # quantization_config=quantization_config
+                                                 )
+    print(f"Model loaded in {time.time() - start_time}s.")
+    return tokenizer, model
+
+TOKENIZER, MODEL = load_model()
+
 
 @app.get("/")
 def get_model_inference(
@@ -26,6 +36,5 @@ def get_model_inference(
 ):
     input_ids = TOKENIZER(input_text, return_tensors="pt").to("mps")
     outputs = MODEL.generate(**input_ids, max_length=1000)
-    # print(TOKENIZER.decode(outputs[0]))
 
     return TOKENIZER.decode(outputs[0])
