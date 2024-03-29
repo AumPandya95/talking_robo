5818607c72030b27567bc9b25c82bc61b882d2c4
Aum Pandya <aumpandya.95@gmail.com>
2024-03-29 18:41:11 +0530

    Loading the model on mps

--- src/main.py
+++ src/main.py
@@ -15,11 +15,11 @@ load_dotenv(env_path)
 def load_model():
     start_time = time.time()
     tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b-it",
-                                            #   device_map="auto",
+                                              device_map="auto",
                                               token=os.environ.get("hugging_face_token")
                                               )
     model = AutoModelForCausalLM.from_pretrained("google/gemma-2b-it",
-                                                #  device_map="auto",
+                                                 device_map="auto",
                                                  token=os.environ.get("hugging_face_token"),
                                                  use_cache=True
                                                  # quantization_config=quantization_config
