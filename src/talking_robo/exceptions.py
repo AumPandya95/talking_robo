c17f164dcd10c29f246ced40826942ec03dbe3bf
Aum Pandya <aumpandya.95@gmail.com>
2024-04-15 03:53:14 +0530

    formatting changes

--- src/talking_robo/exceptions.py
+++ src/talking_robo/exceptions.py
@@ -12,6 +12,7 @@ class RagException(Exception):
             f"Exception message - {self.errors}"
         )
 
+
 class LangchainException(Exception):
     def __init__(self, *args: object, errors: str) -> None:
         super().__init__(*args)
@@ -28,7 +29,7 @@ class LangchainException(Exception):
 class VectorDBException(Exception):
     def __init__(self, *args: object, errors: str) -> None:
         super().__init__(*args)
-        
+
         self.errors = errors
 
     def __str__(self) -> str:
